d22bab15ea707c741990feae519ceece2c8ea094
Troy Lawrence <redirect@gmail.com>
2018-01-23 19:48:55 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,30 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element) {
+   array.unshift(element, ...array);
+   return array;
+}
+
+function destructivelyAddElementToBeginningOfArray (array, element) {
+   array.unshift(element);
+   return array;
+}
+
+function accessElementInArray (array, index) {
+  return array[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray (array) {
+   array.shift();
+   return array;
+}
+
+function RemoveElementFromBeginningOfArray (array) {
+  array.slice(1);
+  return array;
+}
+
+function destructivelyRemoveElementFromEndOfArray (array) {
+  array.pop();
+  return array;
+}
